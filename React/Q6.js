a0631108b6c5593c9eb626c9fccb2de4f072a8e4
Noor <noornabiji4@gmail.com>
2018-10-16 19:58:05 +0530

    Learn About Self-Closing JSX Tags

---
+++ React/Q6.js
@@ -0,0 +1,10 @@
+const JSX = (
+    <div>
+       // remove comment and change code below this line
+      <h2>Welcome to React!</h2> <br/>
+      <p>Be sure to close all tags!</p>
+      <hr/>
+      //remove comment and change code above this line 
+    </div>
+  );
+  
