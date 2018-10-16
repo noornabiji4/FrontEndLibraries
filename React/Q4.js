b2101276c8f7745c098a13c0eeff889659294ea9
Noor <noornabiji4@gmail.com>
2018-10-16 19:51:48 +0530

    Render HTML Elements to the DOM

---
+++ React/Q4.js
@@ -0,0 +1,10 @@
+const JSX = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+  );
+  // change code below this line
+  ReactDOM.render(JSX,document.getElementById('challenge-node'));
+
+  
